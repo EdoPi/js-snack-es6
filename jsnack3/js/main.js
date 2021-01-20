dcd5ad1a1cc6b90fa6d2c90c72b0c995dd6379d1
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-01-20 17:36:58 +0100

    esercizio in ECMA6

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -1,20 +1,47 @@
+/* ES5
 alert ('ciao');
 
-var arrayNum = ['tom','jerry','franco','giulio','francesca','chiara'];
+var myArray = ['tom','jerry','franco','giulio','francesca','chiara'];
 var positionMin = parseInt(prompt('scegli la posizione minima da cui partire tra 1 e 5'));
 var positionMax = parseInt(prompt('scegli la posizione massima tra ' + positionMin + ' e 6'));
-var newArrayNum = [];
+
 
 
 function rangePosition ( array, min , max){
 
+var newArray = [];
+
 for (var i = 0; i < array.length; i++) {
-  if (i >= (min -1 ) && i <= (max-1)) {
+  if (i >= (min-1 ) && i <= (max-1)) {
     console.log(array[i]);
-    newArrayNum.push(array[i]);
+    newArray.push(array[i]);
   }
 }
-  console.log(newArrayNum);
+  return newArray;
 }
 
-rangePosition(arrayNum, positionMin, positionMax);
+rangePosition(myArray, positionMin, positionMax);
+
+*/
+
+
+
+
+//Ecma6
+
+const myArray = ['tom','jerry','franco','giulio','francesca','chiara'];
+const positionMin = parseInt(prompt('scegli la posizione minima da cui partire tra 1 e 5'));
+const positionMax = parseInt(prompt('scegli la posizione massima tra ' + positionMin + ' e 6'));
+
+
+
+function rangePosition ( array, min , max){
+
+  const newArray = array.filter((elemento, index) => {
+    return index >= min && index <= max;
+  })
+  return newArray;
+}
+
+
+console.log(rangePosition(myArray, positionMin, positionMax));
